153b8ad24a75de17f4a83e79111afdc5c2a70f1d
Mustafa <mustafa.jukic@gfn.education>
2022-11-09 21:52:53 +0100

    New JavaDoc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.musti.dydns","l":"DynDNS"},{"p":"de.musti.dydns","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.musti.dydns","l":"DynDNS"},{"p":"de.musti.dydns","l":"GetIP"},{"p":"de.musti.dydns","l":"GetResponseCode"},{"p":"de.musti.dydns","l":"LogBuilder"},{"p":"de.musti.dydns","l":"Main"},{"p":"de.musti.dydns","l":"RunUpdate"}];updateSearchResults();
